8e80c6d0e5cd942678123d4a49ce3783883f07ae
Jacob <jayjayb100@msn.com>
2020-09-08 01:57:52 -0500

    maybe fix

--- commands/src/cmdOrchestratorCommand.js
+++ commands/src/cmdOrchestratorCommand.js
@@ -16,18 +16,9 @@ async function cmdOrchestratorCommand(message, args){
             "body": body
         }
         console.log(options)
-        let url =function(){
-            switch (process.env.environment) {
-                case 'dev':
-                    return process.env.local_orchestrator_url
-                case 'prod':
-                    return process.env.prod_orchestrator_url
-                default:
-                    return process.env.prod_orchestrator_url
-            }
-        }
 
-        request.post(`${url}discord/discord-command`, options, (err, res) => {
+
+        request.post(`${url()}discord/discord-command`, options, (err, res) => {
             console.log(res)
             console.log(err)
             if(res.body.statusCode != 200){
@@ -38,6 +29,17 @@ async function cmdOrchestratorCommand(message, args){
             resolve(res)
         })
     })
+}
+
+function url(){
+    switch (process.env.environment) {
+        case 'dev':
+            return process.env.local_orchestrator_url
+        case 'prod':
+            return process.env.prod_orchestrator_url
+        default:
+            return process.env.prod_orchestrator_url
+    }
 }
 
 module.exports = {cmdOrchestratorCommand}
