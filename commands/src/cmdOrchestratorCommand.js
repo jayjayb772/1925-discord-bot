000af2bcbc705fc584a20130601654891d3f17d2
Jacob <jayjayb100@msn.com>
2020-09-08 03:56:13 -0500

    updated orchestrator service

--- commands/src/cmdOrchestratorCommand.js
+++ commands/src/cmdOrchestratorCommand.js
@@ -9,14 +9,46 @@ async function cmdOrchestratorCommand(message, args){
     })
 }
 
-function handleCommand(cmd, args, resolve, reject){
+function handleCommand(cmd, args, message, resolve, reject){
     switch (cmd) {
         case 'text':
             return
+        case 'display-message':
+            displayMessage(cmd, args, message, resolve, reject)
         default:
             sendInfo(cmd, args, resolve, reject)
 
     }
+}
+
+function options(body){
+    return {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": body
+    }
+}
+
+function displayMessage(cmd, args, message, resolve, reject){
+    let body = JSON.stringify({
+        "cmd":cmd,
+        "message":args.join(' '),
+        "from": message.author.name,
+        "roles":message.author.toJSON()
+    })
+    let options = options(body)
+    console.log(options)
+
+    let uri = url()
+    request.post(`${uri}discord/display-message`, options, (err, res) => {
+        if(res.body.statusCode != 200){
+            console.log(res);
+            reject(res)
+        }
+        console.log(res)
+        resolve(res)
+    })
 }
 
 function sendInfo(cmd, args, resolve, reject){
@@ -24,12 +56,7 @@ function sendInfo(cmd, args, resolve, reject){
         "cmd":cmd,
         "extra":args
     })
-    let options = {
-        "headers": {
-            "content-type": "application/json"
-        },
-        "body": body
-    }
+    let options = options(body)
     console.log(options)
 
     let uri = url()
