102de82524bf13bcae5343e12545c65398e206ff
Jacob <jayjayb100@msn.com>
2020-09-11 19:40:15 -0500

    changed path

--- commands/utils/url.js
+++ commands/utils/url.js
@@ -1,7 +1,7 @@
 function url() {
     return (process.env.environment==='dev') ? (process.env.local_orchestrator_url)
         : ((process.env.environment==='prod') ? (process.env.prod_orchestrator_url)
-            : (process.env.stg_orchestrator_url));
+            : (process.env.prod_orchestrator_url));
 }
 
 module.exports = {url}
